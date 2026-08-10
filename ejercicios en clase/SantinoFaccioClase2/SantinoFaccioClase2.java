d6b353f816621303bd4bdcbce16d567f6e5f16c8
Faccio7L <Santifaccio123@gmail.com>
2026-08-10 16:38:36 -0300

    ejercicios Santino Faccio

---
+++ ejercicios en clase/SantinoFaccioClase2/SantinoFaccioClase2.java
@@ -0,0 +1,9 @@
+public class SantinoFaccioClase2 {
+    public static void main(String[] args){
+     Ejercicio1();
+     Ejercicio2();
+     Ejercicio3();
+    }
+    public static void Ejercicio2(){
+     String array
+    }
