a2818e1dcf882fd4794a9ce34ccd8a159b40ad87
Botita <47467914+Botitaa@users.noreply.github.com>
2026-08-26 21:31:00 -0300

    ejercio de clase numero 6

--- ejercicios en clase/AgustinBotanaClase2/clase3/src/clases/Persona.java
+++ ejercicios en clase/AgustinBotanaClase2/clase3/src/clases/Persona.java
@@ -5,13 +5,17 @@ public class Persona {
     String apellidos;
     String númeroDocumentoIdentidad;
     int añoNacimiento;
+    String paisDeNacimiento;
+    char genero;
 
     Persona(String nombre, String apellidos,
-            String númeroDocumentoIdentidad, int añoNacimiento) {
+            String númeroDocumentoIdentidad, int añoNacimiento, String paisDeNacimiento, char genero) {
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.númeroDocumentoIdentidad = númeroDocumentoIdentidad;
         this.añoNacimiento = añoNacimiento;
+        this.paisDeNacimiento = paisDeNacimiento;
+        this.genero = genero;
     }
 
     void imprimir() {
@@ -19,12 +23,14 @@ public class Persona {
         System.out.println("Apellidos = " + apellidos);
         System.out.println("Número de documento de identidad = " + númeroDocumentoIdentidad);
         System.out.println("Año de nacimiento = " + añoNacimiento);
+        System.out.println("Pais de nacimiento: "+ paisDeNacimiento);
+        System.out.println("Genero: "+ genero);
         System.out.println();
     }
 
     public static void main(String args[]) {
-        Persona p1 = new Persona("Pedro", "Pérez", "1053121010", 1998);
-        Persona p2 = new Persona("Luis", "León", "1053223344", 2001);
+        Persona p1 = new Persona("Pedro", "Pérez", "1053121010", 1998, "Argentina", 'H');
+        Persona p2 = new Persona("Luis", "León", "1053223344", 2001, "Bolivia", 'M');
         p1.imprimir();
         p2.imprimir();
     }
